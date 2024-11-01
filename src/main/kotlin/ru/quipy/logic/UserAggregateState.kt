84889821c25e9d9950ea2c7ee5a278acaf271259
pypkaed <pypka.eduardovna@yandex.ru>
2024-11-01 15:27:35 +0300

    lab6

---
+++ src/main/kotlin/ru/quipy/logic/UserAggregateState.kt
@@ -0,0 +1,25 @@
+package ru.quipy.logic
+
+import ru.quipy.api.*
+import ru.quipy.core.annotations.StateTransitionFunc
+import ru.quipy.domain.AggregateState
+import java.util.*
+
+class UserAggregateState : AggregateState<UUID, UserAggregate> {
+    private lateinit var userId: UUID
+    var createdAt: Long = System.currentTimeMillis()
+    var updatedAt: Long = System.currentTimeMillis()
+
+    lateinit var username: String
+    lateinit var name: String
+
+    override fun getId() = userId
+
+    @StateTransitionFunc
+    fun userRegisteredApply(event: UserRegisteredEvent) {
+        userId = event.userId
+        username = event.username
+        name = event.name
+        updatedAt = createdAt
+    }
+}
