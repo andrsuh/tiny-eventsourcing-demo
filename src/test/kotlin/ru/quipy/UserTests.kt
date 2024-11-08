c2024948c27bf14d080f10e44ea1753bf12ac0e6
petmila <89843525+petmila@users.noreply.github.com>
2024-11-08 17:18:04 +0300

    Add user testcases

--- src/test/kotlin/ru/quipy/UserTests.kt
+++ src/test/kotlin/ru/quipy/UserTests.kt
@@ -1,48 +1,61 @@
 package ru.quipy
 
+import liquibase.hub.model.Project
+import org.junit.Before
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.dropCollection
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
+import org.springframework.test.context.event.annotation.BeforeTestExecution
+import ru.quipy.api.ProjectAggregate
 import ru.quipy.api.UserAggregate
 import ru.quipy.core.EventSourcingService
+import ru.quipy.logic.ProjectAggregateState
 import ru.quipy.logic.UserAggregateState
+import ru.quipy.logic.addProject
 import ru.quipy.logic.create
 import java.util.*
 
 @SpringBootTest
 class UserTests {
 	companion object {
-		private val testId = UUID.randomUUID()
+		private val projectId = UUID.randomUUID()
 		private val userId = UUID.randomUUID()
-		private val login = "D4C"
-		private val password = "DirtyDeedsDoneDirtCheap"
-		private val username = "Valentine"
-
-		private val projectCreator_userId = UUID.randomUUID()
-		private val projectCreatorlogin = "F2M"
-		private val projectCreatorpassword = "MotherMaryMouthsMore"
-		private val projectCreatorusername = "Maria"
+		private val login = "testUser"
+		private val password = "testPassword"
+		private val username = "user"
 	}
 
 	@Autowired
 	lateinit var userEsService: EventSourcingService<UUID, UserAggregate, UserAggregateState>
 
 	@Autowired
+	lateinit var projectEsService: EventSourcingService<UUID, ProjectAggregate, ProjectAggregateState>
+
+	@Autowired
 	lateinit var mongoTemplate: MongoTemplate
 
-	@BeforeEach
-	fun init()
-	{
-		cleanDatabase()
-	}
+//	@Before
+//	fun init()
+//	{
+//		cleanDatabase()
+//	}
 
+	@BeforeTestExecution
 	fun cleanDatabase() {
+		mongoTemplate.dropCollection<UserAggregate>()
+		mongoTemplate.dropCollection(UserAggregate::class.java)
+		mongoTemplate.dropCollection(ProjectAggregate::class.java)
+		mongoTemplate.getCollection("aggregate-user").drop()
+		mongoTemplate.getCollection("aggregate-project").drop()
+//		mongoTemplate.getCollection("snapshots").drop()
 		mongoTemplate.remove(Query.query(Criteria.where("aggregateId").`is`(userId)), "aggregate-user")
+		mongoTemplate.remove(Query.query(Criteria.where("_id").`is`(projectId)), "aggregate-project")
 	}
 
 	@Test
@@ -62,65 +75,46 @@ class UserTests {
 	}
 
 	@Test
-	fun assighProjectToUserTest() {
-		/** Creating project and assigning another user to it */
-		userEsService.create { it.create(projectCreator_userId, projectCreator_login, projectCreator_password, projectCreator_username) }
-		
-		
+	fun assignProjectToUserTest() {
+		val title = "title"
+		val description = "description"
+		userEsService.create { it.create(userId, login, password, username) }
+		projectEsService.create { it.create(projectId, title, description) }
+		val project = projectEsService.getState(projectId)
+
+		userEsService.update(userId) {it.addProject(project)}
 		val state = userEsService.getState(userId)
 
 		Assertions.assertNotNull(state)
 		if (state != null) {
-			Assertions.assertEquals(userId, state.getId())
-			Assertions.assertEquals(login, state.login)
-			Assertions.assertEquals(password, state.password)
-			Assertions.assertEquals(username, state.username)
+			Assertions.assertEquals(1, state.projects.size)
+			Assertions.assertEquals(projectId, state.projects[0])
 		}
 	}
+
 	@Test
-	fun assighProjectToUserTest_NoSuchProject() {
-		/**  */
+	fun assignProjectToUserTest_NoSuchProject() {
 		userEsService.create { it.create(userId, login, password, username) }
-		val state = userEsService.getState(userId)
 
-		userEsService.create { it.create(userId, login, password, username) }
-		val state = userEsService.getState(userId)
-
-		Assertions.assertNotNull(state)
-		if (state != null) {
-			Assertions.assertEquals(userId, state.getId())
-			Assertions.assertEquals(login, state.login)
-			Assertions.assertEquals(password, state.password)
-			Assertions.assertEquals(username, state.username)
+		Assertions.assertThrows(IllegalArgumentException::class.java) {
+			userEsService.update(userId) {it.addProject(null)}
 		}
-		val exception: Exception = assertThrows(IllegalArgumentException::class.java, {
-            throw IllegalArgumentException("exception message")
-        })
-        assertEquals("exception message", exception.message)
 	}
+
 	@Test
-	fun assighProjectToUserTest_NoSuchUser() {
-		/**  */
-		val exception: Exception = assertThrows(IllegalArgumentException::class.java, {
-            throw IllegalArgumentException("exception message")
-        })
-        assertEquals("exception message", exception.message)
-	}
-	@Test
-	fun editProjectTest_UserNotPerformer() {
-		/**  */
-		val exception: Exception = assertThrows(IllegalArgumentException::class.java, {
-            throw IllegalArgumentException("exception message")
-        })
-        assertEquals("exception message", exception.message)
-	}
-	@Test
-	fun assighProjectToUserTest() {
-		/**  */
-	}
-	@Test
-	fun assighProjectToUserTest() {
-		/** User creation with credentials - user has right credentials */
+	fun assignProjectToUserTest_AssignTwice() {
+		val title = "title"
+		val description = "description"
+		userEsService.create { it.create(userId, login, password, username) }
+		projectEsService.create { it.create(projectId, title, description) }
+		val project = projectEsService.getState(projectId)
+		userEsService.update(userId) {it.addProject(project)}
+
+		val state = userEsService.getState(userId)
+
+		Assertions.assertThrows(IllegalArgumentException::class.java) {
+			userEsService.update(userId) {it.addProject(project)}
+		}
 	}
 
 }
