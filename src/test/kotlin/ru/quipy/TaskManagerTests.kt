3d522f285eb5485498a900503266e20488c43350
krugarrr <ttteeework@gmail.com>
2024-11-08 11:03:04 +0300

    feat tests

--- src/test/kotlin/ru/quipy/TaskManagerTests.kt
+++ src/test/kotlin/ru/quipy/TaskManagerTests.kt
@@ -1,13 +1,112 @@
 package ru.quipy
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import ru.quipy.controller.ProjectController
+import ru.quipy.controller.UserController
 
 @SpringBootTest
 class TaskManagerTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Autowired
+    private lateinit var userController: UserController
+
+    @Autowired
+    private lateinit var projectController: ProjectController
+    //@Autowired
+//	lateinit var mongoTemplate: MongoTemplate
+//	@BeforeEach
+//	fun Init() {
+//		cleanDataBase()
+//	}
+//
+//	fun cleanDataBase() {
+//		mongoTemplate.remove(Query.query(Criteria.where()))
+//	}
+
+    @Test
+    fun registerUserAndCompareWithReceivedUser() {
+        val createdUser = userController.registerUser(
+                "krugarrr",
+                "sashulkaterentulka",
+                "ppoklassniypredmet)")
+        val receivedUser = userController.getAccount(createdUser.userId);
+        println()
+        Assertions.assertNotNull(receivedUser)
+        Assertions.assertEquals(createdUser.username, receivedUser?.username)
+        Assertions.assertEquals(createdUser.userId, receivedUser?.getId())
+    }
+
+    @Test
+    fun createProjectAndAddParticipant() {
+        val project = projectController.createProject(
+                "TaskManager",
+                "krugarrr",
+                "sashulkaterentulka",
+                "Very cool and modern project made in USA, Omsk state")
+
+
+		val receivedProject = projectController.getProject(project.projectId)
+        Assertions.assertNotNull(receivedProject)
+		Assertions.assertEquals(1, receivedProject?.participants?.values?.count())
+		Assertions.assertEquals("krugarrr", receivedProject?.participants?.entries?.firstOrNull()?.value?.username)
+
+
+		projectController.addParticipant(
+				project.projectId,
+				"pypkaed",
+				"zarinkamandarinka"
+		)
+
+        val projectWithUpdatedParticipants = projectController.getProject(project.projectId)
+		val newAddedParticipant = projectWithUpdatedParticipants?.participants?.values?.filter { it.username == "pypkaed" }?.first()
+        Assertions.assertEquals("pypkaed", newAddedParticipant?.username)
+        Assertions.assertEquals("zarinkamandarinka", newAddedParticipant?.fullName)
+        Assertions.assertEquals(2, projectWithUpdatedParticipants?.participants?.values?.count())
+    }
+
+    @Test
+    fun createTaskAndChangeStatus() {
+        val project = projectController.createProject(
+                "TaskManager",
+                "krugarrr",
+                "sashulkaterentulka",
+                "Very cool and modern project made in USA, Omsk state")
+
+
+        projectController.createTask(project.projectId, "Watch skibidi guide")
+        val receivedProject = projectController.getProject(project.projectId)
+        val task = receivedProject?.tasks?.entries?.first()?.value
+        val defaultStatus = receivedProject?.taskStatuses?.entries?.first()?.value
+
+        Assertions.assertEquals(1, receivedProject?.tasks?.values?.count())
+        Assertions.assertEquals(1, receivedProject?.taskStatuses?.values?.count())
+        Assertions.assertEquals(defaultStatus?.id, task?.taskStatusesAssigned?.first())
+
+        projectController.createTaskStatus(project.projectId, "Done", "Red")
+        val receivedProjectWithNewStatus = projectController.getProject(project.projectId)
+        val newTaskStatus = receivedProjectWithNewStatus?.taskStatuses?.values?.filter { it.name == "Done" }?.first()
+
+        Assertions.assertEquals(2, receivedProjectWithNewStatus?.taskStatuses?.values?.count())
+        Assertions.assertNotNull(newTaskStatus)
+        Assertions.assertEquals("Red", newTaskStatus?.colour)
+
+        //ну, простите уж)
+        if (newTaskStatus != null) {
+            if (task != null) {
+                projectController.createTaskStatus(project.projectId, newTaskStatus.id, task.id)
+            }
+        }
+
+        //тут непонятно че с методом assign происходит, пока на уточнении, дальше допилю
+//        val receivedProjectWithNewTaskStatus = projectController.getProject(project.projectId)
+//        val updatedTask = receivedProjectWithNewTaskStatus?.tasks?.entries?.first()?.value
+//
+//        Assertions.assertEquals(newTaskStatus?.id, updatedTask?.taskStatusesAssigned?.first())
+
+    }
+
 
 }
