e1726ae349b6add2e73d87bddef3498a2804f67a
petmila <mila-petrenko@inbox.ru>
2024-11-08 15:44:41 +0300

    Update UserTests.kt

--- src/test/kotlin/ru/quipy/UserTests.kt
+++ src/test/kotlin/ru/quipy/UserTests.kt
@@ -22,6 +22,11 @@ class UserTests {
 		private val login = "D4C"
 		private val password = "DirtyDeedsDoneDirtCheap"
 		private val username = "Valentine"
+
+		private val projectCreator_userId = UUID.randomUUID()
+		private val projectCreatorlogin = "F2M"
+		private val projectCreatorpassword = "MotherMaryMouthsMore"
+		private val projectCreatorusername = "Maria"
 	}
 
 	@Autowired
@@ -42,6 +47,7 @@ class UserTests {
 
 	@Test
 	fun createUserTest() {
+		 /** User creation with credentials - user has right credentials */
 		userEsService.create { it.create(userId, login, password, username) }
 
 		val state = userEsService.getState(userId)
@@ -53,6 +59,68 @@ class UserTests {
 			Assertions.assertEquals(password, state.password)
 			Assertions.assertEquals(username, state.username)
 		}
+	}
+
+	@Test
+	fun assighProjectToUserTest() {
+		/** Creating project and assigning another user to it */
+		userEsService.create { it.create(projectCreator_userId, projectCreator_login, projectCreator_password, projectCreator_username) }
+		
+		
+		val state = userEsService.getState(userId)
+
+		Assertions.assertNotNull(state)
+		if (state != null) {
+			Assertions.assertEquals(userId, state.getId())
+			Assertions.assertEquals(login, state.login)
+			Assertions.assertEquals(password, state.password)
+			Assertions.assertEquals(username, state.username)
+		}
+	}
+	@Test
+	fun assighProjectToUserTest_NoSuchProject() {
+		/**  */
+		userEsService.create { it.create(userId, login, password, username) }
+		val state = userEsService.getState(userId)
+
+		userEsService.create { it.create(userId, login, password, username) }
+		val state = userEsService.getState(userId)
+
+		Assertions.assertNotNull(state)
+		if (state != null) {
+			Assertions.assertEquals(userId, state.getId())
+			Assertions.assertEquals(login, state.login)
+			Assertions.assertEquals(password, state.password)
+			Assertions.assertEquals(username, state.username)
+		}
+		val exception: Exception = assertThrows(IllegalArgumentException::class.java, {
+            throw IllegalArgumentException("exception message")
+        })
+        assertEquals("exception message", exception.message)
+	}
+	@Test
+	fun assighProjectToUserTest_NoSuchUser() {
+		/**  */
+		val exception: Exception = assertThrows(IllegalArgumentException::class.java, {
+            throw IllegalArgumentException("exception message")
+        })
+        assertEquals("exception message", exception.message)
+	}
+	@Test
+	fun editProjectTest_UserNotPerformer() {
+		/**  */
+		val exception: Exception = assertThrows(IllegalArgumentException::class.java, {
+            throw IllegalArgumentException("exception message")
+        })
+        assertEquals("exception message", exception.message)
+	}
+	@Test
+	fun assighProjectToUserTest() {
+		/**  */
+	}
+	@Test
+	fun assighProjectToUserTest() {
+		/** User creation with credentials - user has right credentials */
 	}
 
 }
