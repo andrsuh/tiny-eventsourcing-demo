270b77b18d14a914ffd69ed32c4f63b63853e056
Bansai3 <931307@bk.ru>
2024-11-14 22:54:05 +0300

    feat: projections done

--- src/main/kotlin/ru/quipy/projections/AnnotationBasedProjectEventsSubscriber.kt
+++ src/main/kotlin/ru/quipy/projections/AnnotationBasedProjectEventsSubscriber.kt
@@ -3,27 +3,75 @@ package ru.quipy.projections
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
-import ru.quipy.api.ProjectAggregate
-import ru.quipy.api.TagCreatedEvent
-import ru.quipy.api.TaskCreatedEvent
+import ru.quipy.api.*
 import ru.quipy.streams.annotation.AggregateSubscriber
 import ru.quipy.streams.annotation.SubscribeEvent
 
 @Service
 @AggregateSubscriber(
-    aggregateClass = ProjectAggregate::class, subscriberName = "demo-subs-stream"
+    aggregateClass = ProjectAggregate::class, subscriberName = "project-subscriber"
 )
 class AnnotationBasedProjectEventsSubscriber {
 
     val logger: Logger = LoggerFactory.getLogger(AnnotationBasedProjectEventsSubscriber::class.java)
 
     @SubscribeEvent
+    fun projectCreatedSubscriber(event: ProjectCreatedEvent) {
+        logger.info("Project {} with id {} was created by user with id {}",
+            event.title, event.projectId, event.creatorId)
+    }
+
+    @SubscribeEvent
+    fun projectUpdatedSubscriber(event: ProjectUpdatedEvent) {
+        logger.info("Project {} with id {} was updated, update description: {}",
+            event.title, event.projectId, event.description)
+    }
+
+    @SubscribeEvent
+    fun projectUserAddedSubscriber(event: ProjectUserAddedEvent) {
+        logger.info("User with id {} was added to the project with id {}",
+            event.userId, event.projectId)
+    }
+
+    @SubscribeEvent
+    fun projectUserRemovedSubscriber(event: ProjectUserRemovedEvent) {
+        logger.info("User with id {} was removed from the project with id {}",
+            event.userId, event.projectId)
+    }
+
+    @SubscribeEvent
     fun taskCreatedSubscriber(event: TaskCreatedEvent) {
-        logger.info("Task created: {}", event.taskName)
+        logger.info("Task {} with id {} was created in the project with id {}",
+            event.taskName, event.taskId, event.projectId)
+    }
+
+    @SubscribeEvent
+    fun taskUpdatedSubscriber(event: TaskUpdatedEvent) {
+        logger.info("Task {} with id {} in project with id {} was updated",
+            event.taskName, event.taskId, event.projectId)
+    }
+
+    @SubscribeEvent
+    fun taskExecutorAddedSubscriber(event: TaskExecutorAddedEvent) {
+        logger.info("User with id {} became executor of task with id {} in project with id {}",
+            event.userId, event.taskId, event.projectId)
+    }
+
+    @SubscribeEvent
+    fun taskDeletedSubscriber(event: TaskDeletedEvent) {
+        logger.info("Task with id {} was deleted from project with id {}",
+            event.taskId, event.projectId)
     }
 
     @SubscribeEvent
     fun tagCreatedSubscriber(event: TagCreatedEvent) {
-        logger.info("Tag created: {}", event.tagName)
+        logger.info("Tag {} with id {} was created in project with id {}",
+            event.tagName, event.tagId, event.projectId)
+    }
+
+    @SubscribeEvent
+    fun tagAssignedToTaskSubscriber(event: TagAssignedToTaskEvent) {
+        logger.info("Tag with id {} was assigned to task with id {} in project with id {}",
+            event.tagId, event.taskId, event.projectId)
     }
 }
---
+++ src/main/kotlin/ru/quipy/projections/AnnotationBasedUserEventsSubscriber.kt
@@ -0,0 +1,23 @@
+package ru.quipy.projections
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import ru.quipy.api.*
+import ru.quipy.streams.annotation.AggregateSubscriber
+import ru.quipy.streams.annotation.SubscribeEvent
+
+@Service
+@AggregateSubscriber(
+    aggregateClass = UserAggregate::class, subscriberName = "user-subscriber"
+)
+class AnnotationBasedUserEventsSubscriber {
+
+    val logger: Logger = LoggerFactory.getLogger(AnnotationBasedUserEventsSubscriber::class.java)
+
+    @SubscribeEvent
+    fun userCreatedSubscriber(event: UserCreatedEvent) {
+        logger.info("User with id {}, username {}, login {}, password {}",
+            event.userId, event.username, event.login, event.password)
+    }
+}
