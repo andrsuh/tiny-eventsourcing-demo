ed650b542280c21a3a40e12d9d41fb22615309a6
angrydog <vladhober@mail.ru>
2024-11-18 20:19:08 +0300

    swagger

--- src/main/kotlin/ru/quipy/DemoApplication.kt
+++ src/main/kotlin/ru/quipy/DemoApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-open class DemoApplication
+class DemoApplication
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
